a45126cb9bcab5305103527486e7efacc592300e
mobynick <mobynick365@gmail.com>
2021-10-03 23:36:59 +0300

    Рефакторинг операций с матрицами

--- classes/Matrix.py
+++ classes/Matrix.py
@@ -2,273 +2,135 @@ import copy
 
 
 class Matrix:
-    
-    def __init__(self, n, m=False):
-        
+    def __init__(self, n=1, m=1):
         __slots__ = 'matrix'
-# ==========================Создание===========================================     
-        if isinstance(n, list):
-            length = len(n[0])
-#             Задать списком
-            for i in n:
-                assert length == len(i), 'Не прямоугольная матрица'
-                  
-            self.matrix = n
-            del length
-        else:
-#             Задать размером
-            assert n > 0 , 'Неверные данные'
-            assert m > 0 , 'Неверные данные'
-            assert isinstance(n, int), 'Неверные данные'
-            assert isinstance(m, int), 'Неверные данные'
-            
-            if n >= 0 and m >= 0: 
-                self.matrix = [[0 for j in range(m)] for i in range(n)]
-                
-# ========================Транспонирование=====================================   
-    def T(self):
-        
+        assert n > 0, 'Неверные данные'
+        assert m > 0, 'Неверные данные'
+        assert isinstance(n, int), 'Неверные данные'
+        assert isinstance(m, int), 'Неверные данные'
+
+        if n >= 0 and m >= 0:
+            self.n = n
+            self.m = m
+            self.matrix = [[0 for j in range(m)] for i in range(n)]
+
+    @staticmethod
+    def get_from_list(list_):
+        if not Matrix.__check_shape(list_):
+            raise Exception("Ошибка: строки разной длины")
+        matrix = Matrix()
+        matrix.matrix = list_
+        matrix.n = len(list_)
+        matrix.m = len(list_[0])
+        return matrix
+
+    @staticmethod
+    def __check_shape(list_):
+        c = len(list_[0])
+        for i in range(1, len(list_)):
+            if c != len(list_[i]):
+                return False
+        return True
+
+    def transpose(self):
         matrix_T = [[0 for j in range(len(self.matrix))] for i in range(len(self.matrix[0]))]
-        
+
         for i in range(len(self.matrix[0])):
             for j in range(len(self.matrix)):
                 matrix_T[i][j] = self.matrix[j][i]
-        return Matrix(matrix_T)
-    
-# ========================Размер Матрицы=======================================
+
+        return Matrix.get_from_list(matrix_T)
 
     def shape(self):
-        return [len(self.matrix), len(self.matrix[0])]
-    
-# =======================Изменение элементов матрицы===========================
+        return len(self.matrix), len(self.matrix[0])
 
-    def __getitem__(self,key):
+    def __getitem__(self, key):
         return self.matrix[key]
-    
-    def __setitem__(self,key,value):
-        self.matrix[key] = value
-        
 
-# ========================Длина================================================
+    def __setitem__(self, key, value):
+        self.matrix[key] = value
 
     def __len__(self):
         return len(self.matrix)
-    
-# ==========================Сложение=========================================== 
+
+    def __get_op(self, other, op):
+        n, m = self.shape()
+        if isinstance(other, float) or isinstance(other, int):
+            result = [[op(other, self[i][j]) for j in range(m)] for i in range(n)]
+        else:
+            if self.shape() == other.shape():
+                result = [[op(other[i][j], self[i][j]) for j in range(m)] for i in range(n)]
+            else:
+                raise BaseException('Неправильная размерность матрицы')
+        return Matrix.get_from_list(result)
 
     def __add__(self, other):
-        # Копирование для того чтобы не изменять self
-        help_m = copy.deepcopy(self)
-        help_oth = copy.deepcopy(other)
-        # Если число, то каждому элементу + other  
-        if isinstance(other, float) or isinstance(other, int):
-            for i in range(len(self)):
-                for j in range(len(self[0])):
-                    help_m[i][j] = help_m[i][j] + other 
-        # Иначе соответствующие элементы 
-        else:
-            if help_m.shape() == help_oth.shape():
-                for i in range(len(self)):
-                    for j in range(len(self[0])):
-                        help_m[i][j] = help_m[i][j] + help_oth[i][j]
-            else:
-                assert 1 == 0, 'Неправильная размерность матрицы'
-            
-            
-        return help_m
-    
+        return self.__get_op(other, lambda x, y: x + y)
+
     def __radd__(self, other):
-        
-        help_m = copy.deepcopy(self)
-        help_oth = copy.deepcopy(other)
-
-        if isinstance(other, float) or isinstance(other, int):
-            for i in range(len(self)):
-                for j in range(len(self[0])):
-                    help_m[i][j] = help_m[i][j] + other 
-                    
-        return help_m
-    
-# =======================Вычитание=============================================
-
-    def __sub__(self, other):
-        
-        help_m = copy.deepcopy(self)
-        help_oth = copy.deepcopy(other)
-
-        if isinstance(other, float) or isinstance(other, int):
-            for i in range(len(self)):
-                for j in range(len(self[0])):
-                    help_m[i][j] = help_m[i][j] - other 
-                    
-        else:
-            if help_m.shape() == help_oth.shape():
-                for i in range(len(self)):
-                    for j in range(len(self[0])):
-                        help_m[i][j] = help_m[i][j] - help_oth[i][j]
-            else:
-                assert 1 == 0, 'Неправильная размерность матрицы'
-            
-        return help_m
-    
-    def __rsub__(self, other):
-        
-        help_m = copy.deepcopy(self)
-        help_oth = copy.deepcopy(other)
-
-        if isinstance(other, float) or isinstance(other, int):
-            for i in range(len(self)):
-                for j in range(len(self[0])):
-                    help_m[i][j] = other - help_m[i][j]
-                    
-        return help_m
-        
- # =======================Умножение============================================
+        return self + other
 
     def __mul__(self, other):
-        
-        help_m = copy.deepcopy(self)
-        help_oth = copy.deepcopy(other)
-
-        if isinstance(other, float) or isinstance(other, int):
-            for i in range(len(self)):
-                for j in range(len(self[0])):
-                    help_m[i][j] = help_m[i][j] * other 
-                    
-        else:
-            if help_m.shape() == help_oth.shape():
-                for i in range(len(self)):
-                    for j in range(len(self[0])):
-                        help_m[i][j] = help_m[i][j] * help_oth[i][j]
-            else:
-                assert 1 == 0, 'Неправильная размерность матрицы'
-            
-        return help_m
-    
+        return self.__get_op(other, lambda x, y: x * y)
 
     def __rmul__(self, other):
-        
-        help_m = copy.deepcopy(self)
-        help_oth = copy.deepcopy(other)
+        return self * other
 
-        if isinstance(other, float) or isinstance(other, int):
-            for i in range(len(self)):
-                for j in range(len(self[0])):
-                    help_m[i][j] = help_m[i][j] * other 
-                        
-        return help_m
+    def __sub__(self, other):
+        return self + (-1) * other
 
+    def __rsub__(self, other):
+        return self + (-1) * other
 
-# =======================Деление целочисленное=================================
     def __floordiv__(self, other):
-        
-        help_m = copy.deepcopy(self)
-        help_oth = copy.deepcopy(other)
-
-        if isinstance(other, float) or isinstance(other, int):
-            for i in range(len(self)):
-                for j in range(len(self[0])):
-                    help_m[i][j] = help_m[i][j] // other 
-                    
-        else:
-            if help_m.shape() == help_oth.shape():
-                for i in range(len(self)):
-                    for j in range(len(self[0])):
-                        help_m[i][j] = help_m[i][j] // help_oth[i][j]
-            else:
-                assert 1 == 0, 'Неправильная размерность матрицы'
-            
-        return help_m
-    
-
+        return self.__get_op(other, lambda x, y: x // y)
 
     def __rfloordiv__(self, other):
-        
-        help_m = copy.deepcopy(self)
-        help_oth = copy.deepcopy(other)
+        return self // other
 
-        if isinstance(other, float) or isinstance(other, int):
-            for i in range(len(self)):
-                for j in range(len(self[0])):
-                    help_m[i][j] =  other // help_m[i][j]  
-                    
-        return help_m
-
-
-# ================================Деление======================================
     def __truediv__(self, other):
-        
-        help_m = copy.deepcopy(self)
-        help_oth = copy.deepcopy(other)
+        return self.__get_op(other, lambda x, y: x / y)
 
-        if isinstance(other, float) or isinstance(other, int):
-            for i in range(len(self)):
-                for j in range(len(self[0])):
-                    help_m[i][j] = help_m[i][j] / other 
-                    
-        else:
-            if help_m.shape() == help_oth.shape():
-                for i in range(len(self)):
-                    for j in range(len(self[0])):
-                        help_m[i][j] = help_m[i][j] / help_oth[i][j]
-            else:
-                assert 1 == 0, 'Неправильная размерность матрицы'
-            
-        return help_m
-    
     def __rtruediv__(self, other):
-        
-        help_m = copy.deepcopy(self)
-        help_oth = copy.deepcopy(other)
+        return self / other
 
-        if isinstance(other, float) or isinstance(other, int):
-            for i in range(len(self)):
-                for j in range(len(self[0])):
-                    help_m[i][j] = other / help_m[i][j] 
-                    
-         
-        return help_m
-    
-# ====================Нахождение лучшей строки=================================
+    # ====================Нахождение лучшей строки=================================
     def best_line(self):
-        
+
         h_dict = {}
         maxim = 0
         for k, v in enumerate(self):
             maxim = v.count(0)
             sp = [i for i in h_dict.values()]
 
-
             if len(h_dict) != 0:
                 if sp[0][0] == maxim:
-                    h_dict[k] = [maxim , sum(map(abs, v))]
+                    h_dict[k] = [maxim, sum(map(abs, v))]
 
                 if sp[0][0] < maxim:
                     h_dict = {}
                     h_dict[k] = [maxim, sum(map(abs, v))]
             else:
                 h_dict[k] = [maxim, sum(map(abs, v))]
-#         minim = 2**64
-#         for k, v in h_dict.items():
-#             if minim > v[1]:
-#                 minim = v[1]
-#         sp = [k for k, v in h_dict.items() if v[1] != minim ]
-#         for i in sp:
-#             del h_dict[i]
+        #         minim = 2**64
+        #         for k, v in h_dict.items():
+        #             if minim > v[1]:
+        #                 minim = v[1]
+        #         sp = [k for k, v in h_dict.items() if v[1] != minim ]
+        #         for i in sp:
+        #             del h_dict[i]
 
-        return list(h_dict.keys())[0]  
+        return list(h_dict.keys())[0]
 
-
-    # ========================Удаление Элемента=========================
     def __delitem__(self, key):
         del self.matrix[key]
 
-# ======================Рекурсивный определитель===============================
+    # ======================Рекурсивный определитель===============================
     def det(self):
         assert self.shape()[0] == self.shape()[1], 'Не квадратная'
         line = self.best_line()
         summ = 0
-        if self.shape() == [1,1]:
+        if self.shape() == [1, 1]:
             return self[0][0]
         else:
             for ind, elem in enumerate(self[line]):
@@ -277,63 +139,55 @@ class Matrix:
                     for i in range(len(matr)):
                         del matr[i][ind]
                     del matr[line]
-                    summ += ((-1) ** (ind + line)) * (matr.det()) * elem           
+                    summ += ((-1) ** (ind + line)) * (matr.det()) * elem
             return summ
-            
-                
+
     def __str__(self):
         string = ''
         for line in self.matrix:
-        
             string += ', '.join([str(i) for i in line])
             string += '\n'
         return string
 
-    
     # запись матрицы в цсв
     def write_csv(self, path='./data/', file_name='result.csv', delimiter=' '):
-        with open(path+file_name, 'w') as f:
+        with open(path + file_name, 'w') as f:
             for row in self:
                 print(delimiter.join(map(repr, row)), file=f)
         print('Матрица успешно записана!')
 
-
-# скалярное умножение матриц
+    # скалярное умножение матриц
     @staticmethod
     def dot(first, second):
         # считываем кол-во строк и столбцов матриц
-        m1, n1 = first.shape() # m - строки, n - столбцы
+        m1, n1 = first.shape()  # m - строки, n - столбцы
         m2, n2 = second.shape()
         assert n1 == m2, 'Не правильная размерность матриц'
 
         mtrx = Matrix(m1, n2)
-        for i in range(m1): # кол-во строк первой
-            for j in range(n2): # кол-во столбцов второй
+        for i in range(m1):  # кол-во строк первой
+            for j in range(n2):  # кол-во столбцов второй
                 summ = 0
-                for index in range(n1): # n1 == m2
-                    summ += first[i][index] * second[index][j] 
+                for index in range(n1):  # n1 == m2
+                    summ += first[i][index] * second[index][j]
                 mtrx[i][j] = summ
 
         return mtrx
 
-
-
-
-    # считывание матричного уравнения и его решение 
+    # считывание матричного уравнения и его решение
     @staticmethod
     def do_equation(equation: str):
-        alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' 
+        alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
         arr_mtrx = ''
-        for i, elem in enumerate(equation): # считываем вместо букв - матрицы
-            if elem in alph and elem not in arr_mtrx and equation[i+1] != 'a':
+        for i, elem in enumerate(equation):  # считываем вместо букв - матрицы
+            if elem in alph and elem not in arr_mtrx and equation[i + 1] != 'a':
                 arr_mtrx += elem
                 exec(f'{elem}=Matrix.get_by_console("{elem}")')
 
-        try: 
+        try:
             return eval(equation)
         except:
             return 'Посчитать не удалось'
-
 
     # консольное считывание матрицы
     @staticmethod
@@ -344,20 +198,20 @@ class Matrix:
         for i in range(m):
             for j in range(n):
                 # пока принимаем только числа
-                mtrx[i][j] = float(input(f'Введите элемент a({i+1}, {j+1}):\n'))
+                mtrx[i][j] = float(input(f'Введите элемент a({i + 1}, {j + 1}):\n'))
         return mtrx
-
 
     # чтение матрицы с цсв
     @staticmethod
     def read_csv(path='./data/', file_name='matrix.csv', delimiter=' '):
-        return Matrix([[float(token) for token in line.split(delimiter)] for line in open(path+file_name)])
+        return Matrix.get_from_list(
+            [[float(token) for token in line.split(delimiter)] for line in open(path + file_name)]
+        )
 
-    
     # запись матрицы в цсв
     @staticmethod
     def write_matrix_to_csv(matrix, path='./data/', file_name='result.csv', delimiter=' '):
-        with open(path+file_name, 'w') as f:
+        with open(path + file_name, 'w') as f:
             for row in matrix:
                 print(delimiter.join(map(repr, row)), file=f)
         print('Матрица успешно записана!')
--- main.py
+++ main.py
@@ -1,36 +1,5 @@
 from classes import *
 
 
-arr = Matrix([
-            [1,2,9,4],
-            [2,5,9,4],
-            [0,8,7,4],
-            [0,0,1,2]
-])
-
-print('Класс\n',arr)
-print('Размерность', arr.shape())
-print('len() выводит количество строк',len(arr)) 
-print('Можем обращаться к строке и к символу arr[1]:{}, arr[1][0]:{}'.format( arr[1], arr[1][0]))
-print()
-print('сложение и тд +=2\n', arr + 2)
-print('Матрица - матрица\n', arr - arr)
-print('Лучшая строка для определителя', arr.best_line())
-print('Определитель Рекурсией', arr.det())
-
-arr1 = Matrix([
-    [3, -1, 2],
-    [4, 2, 0],
-    [-5, 6, 1]
-])
-
-arr2 = Matrix([
-    [8, 1],
-    [7, 2],
-    [2, -3]
-])
-print(Matrix.dot(arr1, arr2) + 7)
-arr1.write_csv()
-
-
-#print(Matrix.do_equation(input('Вводите матрицы Заглавными латинскими буквами:\n')))
+if __name__ == '__main__':
+    print(Matrix.get_from_list([[1,2,3], [4,5,6]]) - Matrix.get_from_list([[4,5,6], [1,2,3]]))
