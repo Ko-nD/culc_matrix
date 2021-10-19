e0784cd1b64ce306126818c2d4c29ae27e94ad8d
Владимир <69643094+jay-dit@users.noreply.github.com>
2021-10-19 23:40:54 +0300

    Добавил обратную в якоби

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from classes import *
 if __name__ == '__main__':
     # print(Matrix.get_from_list([[1,2,3], [4,5,6]]) - Matrix.get_from_list([[4,5,6], [1,2,3]]))
     # print(Matrix.get_from_list([[1,2,3], [2,5,6], [1, 1, 1]]).det())
-    a = Matrix.get_from_list([[2,1,],[3,4]])
+    a = Matrix.get_from_list([[5,1,],[3,4]])
     other = [[5],[6]]
-    c = 0.01
-    print(a.jacoby(other, c))
-    print(a.revers_matrix())
+    c = 0.001
+    print(a.jacoby(other, c)[1])
+    
